809ce8a9ad996b4dd8fb915a03783cf2d3bc1d89
ry <zijoyrb@gamil.com>
2020-07-13 17:42:18 +0900

    js使えるよう設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,6 +4,28 @@ class OrdersController < ApplicationController
   end
 
   def create
+    @order = Order.new(price: order_params[:price])
+    if @order.valid?
+      pay_item
+      @order.save
+      return redirect_to root_path
+    end
+    render 'index'
+  end
+
+  private
+
+  def order_params
+    params.permit(:price, :token)
+  end
+
+  def pay_item
+    Payjp.api_key = ”PAY.JP秘密鍵”
+    Payjp::Charge.create(
+      amount: order_params[:price],
+      card: order_params[:token],
+      currency:'jpy'
+    )
   end
 
 end
